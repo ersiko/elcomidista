b9d4f39468ca98d411b4686f7ab3310bbe04cd6a
Tomas Nunez <tomas@tomas.cat>
2016-12-14 11:32:06 +0100

    Fixing error with some entries with missing summary, falling back to plain summary

--- comidistabot.py
+++ comidistabot.py
@@ -12,7 +12,7 @@ data = shelve.open("elcomidista_data", writeback=True)
 
 channel_id = sys.argv[2]
 
-for entry in rss['entries'][0:4]:
+for entry in rss['entries']:
     if entry['id'] not in data['sent_messages']:
         message = "*" + entry['title'] + "*[.](" + entry['links'][1]['href'] + ") (por " + entry['author'] + ")"
         tags = []
@@ -21,8 +21,11 @@ for entry in rss['entries'][0:4]:
         message += "\n\n*Etiquetas*: " + ", ".join(tags) + "\n"
         message += entry['summary']
         message += " [Comentarios](" + entry['comments'] + ")"
-        summary_text = bs4.BeautifulSoup(entry['content'][0]['value'], "lxml")
-        message += "\n\n" + summary_text.find_all('p')[0].text
+        try:
+            summary_text = bs4.BeautifulSoup(entry['content'][0]['value'], "lxml")
+            message += "\n\n" + summary_text.find_all('p')[0].text
+        except KeyError: 
+            message += "\n\n" + entry['summary_detail']['value']
         message += " [(Seguir leyendo...)](" + entry['id'] + ")" + "\n\n"
         message += "_Publicado el " + entry['published'] + "_"
         res = bot.sendMessage(channel_id, message, parse_mode="markdown")
